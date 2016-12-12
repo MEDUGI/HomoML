36153dca8b9c738456bb054aea26686accf090ad
ZhangXiangxi <zxx_1996@qq.com>
2016-12-12 09:28:23 +0800

    A full complement of one data provider

--- src/dataInterface/SemeionDataProvider.java
+++ src/dataInterface/SemeionDataProvider.java
@@ -3,7 +3,12 @@ package dataInterface;
 import basicUtils.FeatureExtractor;
 import basicUtils.Matrix;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Xiangxi on 2016/12/6.
@@ -15,6 +20,7 @@ public class SemeionDataProvider implements FeaturedDataProvider{
     protected boolean isReady;
     protected boolean hasFeatured;
     protected Matrix featureMatrix;
+    protected FeatureExtractor featureExtractor;
 
     public SemeionDataProvider(String filepath) {
         isReady = false;
@@ -26,11 +32,46 @@ public class SemeionDataProvider implements FeaturedDataProvider{
         isReady = false;
         hasFeatured = true;
         readFromFile(filepath);
+        this.featureExtractor = featureExtractor;
         featureMatrix = featureExtractor.dataToFeature(dataMatrix);
         isReady = true;
     }
     protected void readFromFile(String filepath) {
-        //TODO: readFromFile
+        try {
+            File file = new File(filepath);
+            ArrayList<ArrayList<Double>> rawData = new ArrayList<>();
+            ArrayList<Double> rawLabel = new ArrayList<>();
+            int lineNumber = 0;
+            if (file.isFile() && file.exists()) {
+                InputStreamReader reader = new InputStreamReader(new FileInputStream(file));
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line = null;
+                while((line = bufferedReader.readLine()) != null) {
+                    String[] fragments = line.split(" ");
+                    lineNumber++;
+                    ArrayList<Double> oneLineData = new ArrayList<>();
+                    for(int i = 0; i < 256; i++) {
+                        oneLineData.add(Double.parseDouble(fragments[i]));
+                    }
+                    rawData.add(oneLineData);
+                    for(int i = 256; i < 266; i++) {
+                        if (Integer.parseInt(fragments[i]) == 1)
+                            rawLabel.add(i-256.0);
+                    }
+                }
+                double[][] data = new double[lineNumber][256];
+                double[][] label = new double[lineNumber][1];
+                for(int i = 0; i < lineNumber; i++) {
+                    for(int j = 0; j < 256; j++)
+                        data[i][j] = rawData.get(i).get(j);
+                    label[i][0] = rawLabel.get(i);
+                }
+                dataMatrix = new Matrix(data);
+                labelMatrix = new Matrix(label);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public Matrix getDataMatrix() {
@@ -59,6 +100,6 @@ public class SemeionDataProvider implements FeaturedDataProvider{
 
     @Override
     public FeatureExtractor getFeatureExtractor() {
-        return null;
+        return featureExtractor;
     }
 }
