340535f3cd03ee38ed54b05bc6eeb3c9a6925817
ZhangXiangxi <zxx_1996@qq.com>
2016-12-28 17:59:46 +0800

    Add the input interface for MNIST dataset

---
+++ src/dataInterface/ImageConvertor.java
@@ -0,0 +1,11 @@
+package dataInterface;
+
+import basicUtils.Matrix;
+
+/**
+ * Created by Xiangxi on 2016/12/28.
+ * Contact him on zxx_1996@qq.com
+ */
+public interface ImageConvertor {
+    Matrix toImageMatrix(Matrix rowMatrix);
+}
---
+++ src/dataInterface/ImageDataProvider.java
@@ -0,0 +1,9 @@
+package dataInterface;
+
+/**
+ * Created by Xiangxi on 2016/12/28.
+ * Contact him on zxx_1996@qq.com
+ */
+public abstract class ImageDataProvider implements DataProvider, ImageConvertor{
+
+}
